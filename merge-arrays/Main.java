eeb104c3a2fda55525b5fa7bc635f1edefb48d92
Kaosc <kaoscly@gmail.com>
2025-09-15 21:21:26 +0300

    Refactor merge method to return ArrayList and improve duplicate handling; add .gitignore for class files

--- merge-arrays/Main.java
+++ merge-arrays/Main.java
@@ -2,22 +2,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-
-   public static String[] merge(String[] list1, String[] list2) {
+   
+   public static ArrayList<String> merge(String[] list1, String[] list2) {
       ArrayList<String> mergedList = new ArrayList<>(Arrays.asList(list1));
-      mergedList.addAll(Arrays.asList(list2));
+      ArrayList<String> mergedList2 = new ArrayList<>(Arrays.asList(list2));
+      
 
       for (int i = 0; i < mergedList.size(); i++) {
-         for (int j = i + 1; j < mergedList.size(); j++) {
-            if (mergedList.get(i).equals(mergedList.get(j))) {
-               mergedList.remove(j);
-               j--; // to avoid skipping the next element
+         System.out.println("i:" + i);
+         System.out.println("----------------");
+
+         for (int j = 0; j < mergedList2.size(); j++) {
+            System.out.println("j:" + j);
+
+            if (mergedList.get(i).equals(mergedList2.get(j))) {
+               mergedList2.remove(j);
             }
          }
 
+         System.out.println("----------------");
       }
 
-      return mergedList.toArray(new String[0]);
+      mergedList.addAll(mergedList2);
+      return mergedList;
    }
 
    public static void main(String[] args) {
@@ -25,7 +32,7 @@ public class Main {
       String[] shoppingList1 = { "milk", "bread", "egg", "butter", "sugar" };
       String[] shoppingList2 = { "milk", "Bread", "EGG", "butter", "sugar" };
 
-      String[] uniqShoppingList = merge(shoppingList1, shoppingList2);
+      ArrayList<String> uniqShoppingList = merge(shoppingList1, shoppingList2);
 
       System.out.println("\nShopping List: \n");
       for (String item : uniqShoppingList) {
