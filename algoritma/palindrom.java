7b9f6c78c1273a83e1d3b556bb989fb3da0bda92
Kaosc <kaoscly@gmail.com>
2026-03-24 20:29:38 +0300

    update

---
+++ algoritma/palindrom.java
@@ -0,0 +1,31 @@
+package algoritma;
+
+public class palindrom {
+   static String[] splitString(String str) {
+      String[] arr = str.split(" ");
+      return arr;
+   }
+
+   static int isPalindrome(String str) {
+      String[] arr = splitString(str);
+      int palindromeCount = 0;
+
+      for (String word : arr) {
+         String reversed = "";
+
+         for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+         }
+
+         if (word.equals(reversed)) {
+            palindromeCount++;
+         }
+      }
+
+      return palindromeCount;
+   }
+
+   public static void main(String[] args) {
+      System.out.println("Palindrom kelime sayısı = " + isPalindrome("racecar hello ama")); // 2
+   }
+}
