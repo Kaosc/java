8887f0b7f2cf6e72e5a6adc3de95ea97d0022f82
Kaosc <kaoscly@gmail.com>
2026-03-29 17:52:57 +0300

    update

---
+++ algoritma/methods/M4.java
@@ -0,0 +1,37 @@
+package algoritma.methods;
+
+import java.util.Scanner;
+
+public class M4 {
+
+	public void ort(double[] v, double[] f) {
+		double sonuc;
+		double[] ort = new double[v.length];
+		for (int i = 0; i < v.length; i++) {
+			sonuc = (v[i] * 0.4) + (f[i] * 0.6);
+			ort[i] = sonuc;
+
+		}
+		for (double m : ort) {
+			System.out.println(m);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		double[] vize = new double[n];
+		double[] fin = new double[n];
+
+		for (int i = 0; i < vize.length; i++) {
+
+			vize[i] = s.nextDouble();
+			fin[i] = s.nextDouble();
+		}
+
+		M4 m = new M4();
+		m.ort(vize, fin);
+		s.close();
+	}
+
+}
---
+++ algoritma/methods/M6.java
@@ -0,0 +1,51 @@
+package algoritma.methods;
+
+import java.util.Scanner;
+
+public class M6 {
+
+	/*
+	 * boyutu ve elemanları dışarıdan girilen
+	 * bir double dizinin ortalamasını diziolutur,
+	 * dtopla ve dortalama olmak üzere
+	 * 3 ayrı geriye değer döndünrem emtotla yapınız.
+	 */
+
+	public double[] olustur(int n) {
+		Scanner s = new Scanner(System.in);
+		double d[] = new double[n];
+		for (int i = 0; i < n; i++) {
+			d[i] = s.nextDouble();
+		}
+		s.close();
+		return d;
+	}
+
+	public double topla(double[] y) {
+		double tpl = 0;
+		for (double m : y) {
+			tpl = tpl + m;
+		}
+		return tpl;
+
+	}
+
+	public double ort(double t, int n) {
+		double ort = t / n;
+		return ort; // return t/n;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		M6 m = new M6();
+		double b[] = m.olustur(s.nextInt());
+		s.close();
+		System.out.println(m.ort(m.topla(b), b.length));
+		// double tp=m.topla(b);
+		// double y=m.ort(tp,b.length);
+		// double y=m.ort(m.topla(b),b.length);
+		// System.out.println(y);
+
+	}
+
+}
