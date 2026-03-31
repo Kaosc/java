68c07ff2391a87f2b2978139c7415eb1fc9858ac
Kaosc <kaoscly@gmail.com>
2026-03-31 11:50:47 +0300

    update

---
+++ algoUygulamalar/uygulamafoy5/melihrusenozkulFoy5.java
@@ -0,0 +1,10 @@
+package algoUygulamalar.uygulamafoy5;
+
+import java.util.Scanner;
+
+public class melihrusenozkulFoy5 {
+   public static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+   }
+}
---
+++ algoUygulamalar/uygulamafoy6/melihrusenozkulFoy6.java
@@ -0,0 +1,10 @@
+package algoUygulamalar.uygulamafoy6;
+
+import java.util.Scanner;
+
+public class melihrusenozkulFoy6 {
+   public static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) {
+   }
+}
