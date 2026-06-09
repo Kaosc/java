c12539968e13517b574cf29d239ad69d72dc0361
Kaosc <kaoscly@gmail.com>
2026-06-09 08:15:33 +0300

    UPDATE

--- algoritma/swing/Label.java
+++ algoritma/swing/Label.java
@@ -2,6 +2,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
 
 public class Label {
    public static void main(String[] args) {
@@ -12,6 +15,49 @@ public class Label {
       label.setBounds(5, 5, 135, 40);
       label.setBackground(Color.BLACK);
       label.setOpaque(true); // display background color of label
+      label.setFocusable(true); // allow label to receive focus and key events
+
+      label.addKeyListener(new KeyAdapter() {
+         @Override
+         public void keyPressed(KeyEvent e ) {
+            System.out.println("Key pressed");
+         }
+         @Override
+         public void keyReleased(KeyEvent e) {
+            System.out.println("Key released");
+         }  
+         @Override
+         public void keyTyped(KeyEvent e) {
+            System.out.println("Key typed");
+         }
+      });
+
+      label.addMouseListener(new MouseListener() {
+         @Override
+         public void mouseClicked(java.awt.event.MouseEvent e) {
+            System.out.println("Mouse clicked!");
+         }
+
+         @Override
+         public void mousePressed(java.awt.event.MouseEvent e) {
+            System.out.println("Mouse pressed!");
+         }
+
+         @Override
+         public void mouseReleased(java.awt.event.MouseEvent e) {
+            System.out.println("Mouse released!");
+         }
+
+         @Override
+         public void mouseEntered(java.awt.event.MouseEvent e) {
+            System.out.println("Mouse entered!");
+         }
+
+         @Override
+         public void mouseExited(java.awt.event.MouseEvent e) {
+            System.out.println("Mouse exited!");
+         }
+      });
 
       ///////////////////////////////////////////////////////////////////////////
 
