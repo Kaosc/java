b56dbee6b2f7c673c47e739be48e9b84a658f8fb
Kaosc <kaoscly@gmail.com>
2026-06-10 20:13:30 +0300

    update

--- oop/threads/Example3.java
+++ oop/threads/Example3.java
@@ -1,5 +1,9 @@
 package oop.threads;
 
+// synchronized keyword is used to control access to a particular resource by multiple threads. When a method or block of code is marked as synchronized, only one thread can access it at a time. This is particularly useful when you have shared resources (class) that multiple threads need to access, such as a shared counter, a shared list, or in this case, a shared pool of tickets.
+
+
+// Define a class that represents a pool of tickets. This class will have a method to sell tickets, and we will use the synchronized keyword to ensure that only one thread can sell a ticket at a time.
 class TicketPool {
    private int totalTickets = 5;
 
@@ -17,6 +21,7 @@ class TicketPool {
    }
 }
 
+// Define the task that will be executed by each thread. This task will try to buy a ticket from the TicketPool. This is a task that will be executed by multiple threads concurrently, and it will access the shared resource (TicketPool) to sell tickets.
 class BuyTicketTask implements Runnable {
    private TicketPool ticketPool;
    private String customerName;
@@ -36,6 +41,8 @@ public class Example3 {
       TicketPool ticketPool = new TicketPool();
       System.err.println(ticketPool.getTotalTickets() + " ticket available to sell.");
 
+      // Define tasks that will try to access the shared resource (TicketPool)
+      // concurrently
       BuyTicketTask g1 = new BuyTicketTask(ticketPool, "Kaosc");
       BuyTicketTask g2 = new BuyTicketTask(ticketPool, "Hyle");
       BuyTicketTask g3 = new BuyTicketTask(ticketPool, "Morph");
@@ -43,6 +50,7 @@ public class Example3 {
       BuyTicketTask g5 = new BuyTicketTask(ticketPool, "Scream");
       BuyTicketTask g6 = new BuyTicketTask(ticketPool, "Guardian");
 
+      // Create threads for each task
       Thread t1 = new Thread(g1);
       Thread t2 = new Thread(g2);
       Thread t3 = new Thread(g3);
@@ -50,10 +58,23 @@ public class Example3 {
       Thread t5 = new Thread(g5);
       Thread t6 = new Thread(g6);
 
+      // So tecnically, all threads will start at the same time and try to access the
+      // shared resource (TicketPool) concurrently. So you might think that the output
+      // will be random and some threads might not get a chance to buy a ticket
+      // because of the race condition. However, because we have implemented the
+      // synchronized keyword in the sellTicket() method of the TicketPool class, it
+      // will not allow multiple threads to access the method at the same time. Only
+      // one thread can access the method at a time, and the others will have to WAIT
+      // until the current thread finishes its execution. This means that even though
+      // all threads start at the same time, they will access the sellTicket() method
+      // one by one in a sequential manner, ensuring that each thread gets a chance to
+      // buy a ticket until there are no tickets left.
+
       // Will start at the same time but because of the synchronized key we
-      // implemented to TicketPool.sellTicket() method, it will not allow the
-      // thread to access the TicketPool class and execute the sellTicket method.
-      // It'll have to await for the previous process to end.
+      // implemented to TicketPool.sellTicket() method, it will not allow the thread
+      // to access the TicketPool class and execute the sellTicket method. It'll have
+      // to await for the previous process to end. It's like a queue. So the output
+      // will be in order of the threads we started.
       t1.start();
       t2.start();
       t3.start();
--- oop/threads/Threads.java
+++ oop/threads/Threads.java
@@ -1,6 +1,18 @@
 package oop.threads;
 
-// Runnable arayüzünü uygulayan sınıfımız
+// Threads are bassically independent paths of execution in a program. They allow you to perform multiple tasks concurrently, which can improve the performance of your application, especially when dealing with I/O operations or long-running tasks.
+
+// Runnable's are the tasks that we want to run in separate threads. They are defined by implementing the Runnable interface, which requires us to implement the run() method. This method contains the code that will be executed when the thread is started.
+
+// Thread is the actual thread of execution. We can create a Thread object by passing a Runnable object to its constructor. Once we have a Thread object, we can start it using the start() method, which will call the run() method of the Runnable in a new thread of execution.
+
+// So like basic flow is;
+// 1. Create a Runnable object that defines the task to be performed.
+// 2. Create a Thread object and pass the Runnable object to its constructor.
+// 3. Start the thread using the start() method, which will execute the run() method of the Runnable in a new thread of execution.
+
+// Note: We can also use static class called sleep() method like Thread.sleep(milliseconds) to pause the execution of a thread for a specified amount of time. This can be useful for simulating delays or waiting for certain conditions to be met before proceeding with the execution of the thread. The method can be called from within the run() method of a Runnable, from any thread, or even from anywhere of the program. However, it is important to handle the InterruptedException that may be thrown when a thread is interrupted while sleeping.
+
 class TekSayiYazdirici implements Runnable {
     private String threadAdi;
 
@@ -13,6 +25,13 @@ class TekSayiYazdirici implements Runnable {
         System.out.println("--- " + threadAdi + " Basladi ---");
         for (int i = 1; i < 20; i += 2) {
             System.out.println(threadAdi + ": " + i);
+
+            // Must be in a try-catch block because sleep() can throw InterruptedException
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(threadAdi + " interrupted: " + e.getMessage());
+            }
         }
         System.out.println("--- " + threadAdi + " Bitti ---\n");
     }
@@ -20,33 +39,28 @@ class TekSayiYazdirici implements Runnable {
 
 public class Threads {
     public static void main(String[] args) {
-        // Görevlerin (Runnable) oluşturulması
         TekSayiYazdirici gorev1 = new TekSayiYazdirici("Thread 1");
         TekSayiYazdirici gorev2 = new TekSayiYazdirici("Thread 2");
         TekSayiYazdirici gorev3 = new TekSayiYazdirici("Thread 3");
 
-        // Thread nesnelerinin oluşturulması
         Thread t1 = new Thread(gorev1);
         Thread t2 = new Thread(gorev2);
         Thread t3 = new Thread(gorev3);
 
         try {
-            // Önce ilk thread çalışır ve bitmesi beklenir
-            t1.start();
-            t1.join(); // t1 bitene kadar ana programı (main) bekletir
+            t1.start(); // Start the first thread
+            t1.join(); // Wait for the first thread to finish before starting the second thread
 
-            // Sonra ikinci thread çalışır ve bitmesi beklenir
             t2.start();
-            t2.join(); // t2 bitene kadar bekler
+            t2.join();
 
-            // En son üçüncü thread çalışır ve bitmesi beklenir
             t3.start();
-            t3.join(); // t3 bitene kadar bekler
+            t3.join();
 
         } catch (InterruptedException e) {
-            System.out.println("Thread kesintiye ugradi: " + e.getMessage());
+            System.out.println("Thread interrupted: " + e.getMessage());
         }
 
-        System.out.println("Tum thread'ler sirayla calismasini tamamladi.");
+        System.out.println("All threads have finished execution.");
     }
 }
